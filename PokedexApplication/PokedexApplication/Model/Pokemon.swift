cdb4d20ebe888849ce4305569497807464b58d9e
Edgars Vanags <edgars.vanags@testdevlab.com>
2017-10-02 17:52:53 +0300

    Created custom collection cell, did set up view controller

---
+++ PokedexApplication/PokedexApplication/Model/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  PokedexApplication
+//
+//  Created by Edgars Vanags on 02/10/2017.
+//  Copyright © 2017 edgarsvanags. All rights reserved.
+//
+
+import Foundation
---
+++ PokedexApplication/PokedexApplication/View/PokemonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonCell.swift
+//  PokedexApplication
+//
+//  Created by Edgars Vanags on 02/10/2017.
+//  Copyright © 2017 edgarsvanags. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCell: UICollectionViewCell {
+    
+}
