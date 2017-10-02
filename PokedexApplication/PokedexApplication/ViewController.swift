a0ae4a34a631960e0cfdce99dd6fcff87d35aeea
Edgars Vanags <edgars.vanags@testdevlab.com>
2017-10-02 15:52:20 +0300

    Adding project files

---
+++ PokedexApplication/PokedexApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokedexApplication
+//
+//  Created by Edgars Vanags on 02/10/2017.
+//  Copyright © 2017 edgarsvanags. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
