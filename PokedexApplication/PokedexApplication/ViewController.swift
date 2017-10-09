b6ead7a5a6254c7789e7128a9ad1b1c20fdad4bb
Edgars Vanags <edgars.vanags@testdevlab.com>
2017-10-09 22:12:02 +0300

    Added custom pokemon font and music to application

--- PokedexApplication/PokedexApplication/ViewController.swift
+++ PokedexApplication/PokedexApplication/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 	
 	var pokemon = [Pokemon]()
+	var musicPlayer: AVAudioPlayer!
 	
 	@IBOutlet weak var collectionView: UICollectionView!
 	
@@ -18,6 +20,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 		super.viewDidLoad()
 		
 		parsePokemonCSV()
+		initAudio()
 	}
 	
 	func parsePokemonCSV() {
@@ -32,6 +35,22 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 						let pokemon = Pokemon(name: name, pokedexId: pokedexId)
 						self.pokemon.append(pokemon)
 					}
+				}
+			} catch let err as NSError {
+				print(err.debugDescription)
+			}
+		}
+	}
+	
+	func initAudio() {
+		if let path = Bundle.main.path(forResource: "music", ofType: "mp3") {
+			
+			do {
+				if let url = URL(string: path) {
+					musicPlayer = try AVAudioPlayer(contentsOf: url)
+					musicPlayer.prepareToPlay()
+					musicPlayer.numberOfLoops = -1
+					musicPlayer.play()
 				}
 			} catch let err as NSError {
 				print(err.debugDescription)
@@ -60,6 +79,17 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 	
 	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 		return CGSize(width: 100, height: 100)
+	}
+	
+	@IBAction func musicButtonPressed(_ sender: UIButton) {
+		//TODO: add UserDefaults to save if music was on or off
+		if musicPlayer.isPlaying {
+			musicPlayer.pause()
+			sender.alpha = 0.2
+		} else {
+			musicPlayer.play()
+			sender.alpha = 1.0
+		}
 	}
 }
 
